6ac2845fbd721a7571bd782b44258693a7eef070
Larry Davis <lazdnet@gmail.com>
2014-04-26 10:36:06 -0700

    Send images on an interval instead of pounding the client with a stream, fixes memory and lag issues but still not good enough

--- server/cameraServer.js
+++ server/cameraServer.js
@@ -8,6 +8,7 @@ module.exports = function(options) {
 
   var device = options.device || 1;
   var port = options.port || 3002;
+  var fps = options.fps || 4;
   var connections = [];
 
   // Send each image to all connected clients
@@ -41,7 +42,7 @@ module.exports = function(options) {
 
     // Start streaming when someone connects
     // @todo don't make this call unless the stream is paused
-    cameraStream.resume();
+    startPolling();
 
     res.connection.on('close', function() {
       log('camServer: client disconnected from %s', ip);
@@ -51,7 +52,7 @@ module.exports = function(options) {
 
       // Stop streaming if nobody is connected
       if (connections.length === 0) {
-        cameraStream.pause();
+        stopPolling();
       }
     });
   });
@@ -59,13 +60,39 @@ module.exports = function(options) {
   server.listen(port);
   log('camServer: Started on port %d', port);
 
-  // Create a camera capture stream
-  var camera = new cv.VideoCapture(device);
-  var cameraStream = camera.toStream();
-  var buffer = new Buffer(0);
-  cameraStream.on('data', function(image) {
-    buffer = image.toBuffer();
+  function startPolling() {
+    if (!pollCamera) {
+      pollCamera = true;
+      getImage();
+    }
+  }
 
-    sendToAll();
-  });
+  function stopPolling() {
+    pollCamera = false;
+    clearTimeout(pollTimeout);
+  }
+
+  // Create a camera capture stream
+  var pollCamera = false;
+  var camera = new cv.VideoCapture(device);
+  var buffer = new Buffer(0);
+
+  var pollTimeout;
+  function getImage() {
+    camera.read(function(err, image) {
+      if (err) {
+        log('camServer: Error reading image from camera %s', err);
+        return;
+      }
+
+      buffer = image.toBuffer();
+
+      sendToAll();
+
+      // Do next poll
+      if (pollCamera) {
+        pollTimeout = setTimeout(getImage, 1000 / fps);
+      }
+    });
+  }
 };
