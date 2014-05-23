067aed2fb1d41527d5d97d9b9fff92f5af041f18
Larry Davis <lazdnet@gmail.com>
2014-05-23 10:22:21 -0700

    Slightly less stupid autodrive

--- server/autodrive.js
+++ server/autodrive.js
@@ -2,6 +2,7 @@ var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 var log = require('./logger');
 var constants = require('./constants');
+var Q = require('q');
 
 var modes = {
   reverse: -1,
@@ -9,14 +10,35 @@ var modes = {
   forward: 1,
   turnLeft: 2,
   turnRight: 3,
-  turnTo: 4
+  turnTo: 4,
+  backOut: 5,
+  turnToClearPath: 6
 };
+
+function getTurnMode(current, target) {
+  if ((target - current + 360) % 360 > 180) {
+    return modes.turnLeft;
+  }
+  else {
+    return modes.turnRight;
+  }
+}
+
+function getHeading(current, offset) {
+  return Math.abs(current + offset) % 360;
+}
 
 var AutoDrive = module.exports = function(controller) {
   EventEmitter.call(this);
 
   // Store reference to controller
   this.controller = controller;
+
+  // Start out going forward
+  this.mode = modes.forward;
+
+  // Be a reactive creature
+  this.loop = this.react;
 };
 
 util.inherits(AutoDrive, EventEmitter);
@@ -34,13 +56,11 @@ AutoDrive.prototype.forward = function() {
 };
 
 AutoDrive.prototype.turnLeft = function() {
-  this.controller.setState(constants.rc.center, constants.rc.center + constants.autonomy.steer);
-  this.lastTurn = modes.turnLeft;
+  this.controller.setState(constants.rc.center, constants.rc.center - constants.autonomy.steer);
 };
 
 AutoDrive.prototype.turnRight = function() {
-  this.controller.setState(constants.rc.center, constants.rc.center - constants.autonomy.steer);
-  this.lastTurn = modes.turnRight;
+  this.controller.setState(constants.rc.center, constants.rc.center + constants.autonomy.steer);
 };
 
 AutoDrive.prototype.applyMode = function(mode) {
@@ -61,7 +81,7 @@ AutoDrive.prototype.applyMode = function(mode) {
       this.brake();
       break;
     default:
-      log('Invalid mode %s', mode);
+      throw new Error('Cannot set mode ' + mode);
   }
 }
 
@@ -83,42 +103,148 @@ AutoDrive.prototype.getCollisions = function() {
   };
 };
 
-AutoDrive.prototype.update = function(state) {
-  this.state = state;
+// Back up until we have no collisions
+AutoDrive.prototype.backOut = function(cb) {
+  log('Backing out...');
 
-  // Test for impending collisions
-  var collisions = this.getCollisions();
+  this.mode = modes.backOut;
 
- if (collisions.centerCollide) {
-    // Both sensors or middle sensor detects a wall, we can't go forward
+  var deferred = Q.defer();
 
-    // If last mode was left turn, turn left
-    if (this.lastTurn === modes.turnLeft && !collisions.leftCollide) {
-      log('Keep turning left...');
-      this.mode = modes.turnLeft;
-    }
-    else if (this.lastTurn === modes.turnRight && !collisions.rightCollide) {
-      log('Keep turning right...');
-      this.mode = modes.turnRight;
+  // On the next tick, we'll back out
+  var doBackOut = function() {
+    var collisions = this.collisions;
+    if (!collisions.leftCollide || !collisions.rightCollide) {
+      log('Done backing out!');
+      // When one side is free, we're done
+      this.mode = null;
+      deferred.resolve();
     }
     else {
-      log('Reverse!');
-      this.mode = modes.reverse;
+      // log('Backing out...');
+      // Otherwise, keep backing out
+      this.reverse();
     }
+  };
+
+  this.loop = doBackOut;
+
+  return deferred.promise;
+};
+
+AutoDrive.prototype.turnToClearPath = function() {
+  log('Looking for clear path...');
+
+  var clearPathDist = constants.autonomy.collisionDist * 2;
+
+  var deferred = Q.defer();
+
+  var collisions = this.collisions;
+
+  // If there's no collision, return
+  if (this.state.centerDist > clearPathDist) {
+    log('Already clear!');
+    deferred.resolve();
+
+    return deferred.promise;
+  }
+
+  if (collisions.leftCollide && collisions.rightCollide) {
+    deferred.reject(new Error("Can't turn, neither direction is clear!"));
+    return false;
+  }
+
+  this.mode = modes.turnToClearPath;
+
+  // Turn left by default
+  var mode = modes.turnLeft;
+
+  // If there's something blocking the left, turn right
+  if (collisions.leftCollide) {
+    mode = modes.turnRight;
+  }
+
+  var doTurn = function() {
+    // var collisions = this.collisions;
+
+    // Stop turning after time
+    if (this.state.centerDist > clearPathDist) {
+      log('Clear path found!');
+      this.loop = null;
+      deferred.resolve();
+    }
+    else {
+      log('Turning %d...', this.state.centerDist);
+      this.applyMode(mode);
+    }
+  };
+
+  // On the next tick, we'll start turning
+  this.loop = doTurn;
+
+  return deferred.promise;
+};
+
+AutoDrive.prototype.react = function() {
+  var self = this;
+  var collisions = this.collisions;
+
+  // if (collisions.centerCollide && collisions.leftCollide && collisions.rightCollide) {
+  if (collisions.leftCollide && collisions.rightCollide) {
+    // No clear path, can't turn or go forward
+    return this.backOut()
+      .then(
+        function() {
+          log('Back out complete! Will turn around now...');
+          return self.turnToClearPath();
+        },
+        function(err) {
+          log(err+', will try to resume operations..');
+          self.mode = modes.forward;
+          self.loop = self.react;
+        }
+      )
+      .then(
+        function() {
+          log('Backout and turn both complete, resuming react mode...');
+          self.mode = modes.forward;
+          self.loop = self.react;
+        }
+      )
+      .catch(function(error) {
+        self.loop = null;
+        log(err + ", don't know what to do!");
+      });
+  }
+  else if (collisions.centerCollide) {
+    return this.turnToClearPath()
+      .then(
+        function() {
+          log('Clear path found, resuming react mode...');
+          self.mode = modes.forward;
+          self.loop = self.react;
+        }
+      )
+      .catch(function(error) {
+        self.loop = null;
+        log(err + ", don't know what to do!");
+      });
   }
   else if (collisions.rightCollide) {
-    // Turn left
+    // Imminent collision on the right
     if (this.mode != modes.turnLeft) {
       log('Turn left...');
     }
-    this.mode = modes.turnRight;
+    this.mode = modes.turnLeft;
+    this.applyMode(this.mode);
   }
   else if (collisions.leftCollide) {
-    // Turn right
+    // Imminent collision on the left
     if (this.mode != modes.turnRight) {
       log('Turn right...');
     }
-    this.mode = modes.turnLeft;
+    this.mode = modes.turnRight;
+    this.applyMode(this.mode);
   }
   else {
     // Go forward
@@ -126,7 +252,19 @@ AutoDrive.prototype.update = function(state) {
       log('Forward!');
     }
     this.mode = modes.forward;
+    this.applyMode(this.mode);
   }
+};
 
-  this.applyMode(this.mode);
+AutoDrive.prototype.update = function(state) {
+  // Store the state
+  this.state = state;
+
+  // Test for impending collisions
+  // Run this before the loop
+  this.collisions = this.getCollisions();
+
+  if (!state.batteryDead && this.loop) {
+    this.loop();
+  }
 };
